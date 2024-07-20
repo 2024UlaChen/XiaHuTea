8fba3e76d374fccfcfcf14d04970771e540dd85e
May <may.chen82612090@gmail.com>
2024-07-20 12:36:44 +0800

    create project

---
+++ src/main/java/idv/tia201/g2/XiahuteaApplication.java
@@ -0,0 +1,13 @@
+package idv.tia201.g2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiahuteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiahuteaApplication.class, args);
+	}
+
+}
