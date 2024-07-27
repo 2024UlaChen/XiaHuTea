ac410ac4c29b271820f2f2b72237e735852cdf34
May <may.chen82612090@gmail.com>
2024-07-27 11:00:25 +0800

    involve project

--- src/main/java/idv/tia201/g2/XiahuteaApplication.java
+++ src/main/java/idv/tia201/g2/XiahuteaApplication.java
@@ -2,9 +2,11 @@ package idv.tia201.g2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class XiahuteaApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+	public class XiahuteaApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(XiahuteaApplication.class, args);
---
+++ src/main/resources/static/assets/js/newjs.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function (e) {
+    // const aDropList = document.g
+})
